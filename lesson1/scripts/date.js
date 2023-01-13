2bd3c402acad31d75107dedf5578cd6c4e4e50c7
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-01-13 16:02:33 -0700

    made js date

---
+++ lesson1/scripts/date.js
@@ -0,0 +1,48 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+
+document.getElementById("currentdate").textContent = fulldate;
+
+//******************************* */
+try {
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    document.getElementById(
+        "currentdate2"
+    ).textContent = new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+    alert("Error with code or your browser does not support Locale");
+}
+alert(document.lastModified);
+let oLastModif = new Date(document.lastModified);
