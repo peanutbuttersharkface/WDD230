1dccb3178183ed71a82fd890d12116ecb496d22f
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-01-25 17:35:54 -0700

    updated chamber site

---
+++ lesson3/scripts/script.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear()
+const lastModified = document.lastModified
+
+document.querySelector("#currentYear").textContent = year
+document.querySelector("#lastUpdated").textContent = lastModified
