ff075aa277e5406b227c10ffa096e965cbe0781b
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-03-09 21:36:20 -0700

    update weather activity

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -21,7 +21,7 @@ apiFetch();
 function displayResults(weatherData){
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
 
-    const iconsrc ='https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png';
+    const iconsrc =`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
 
     weatherIcon.setAttribute('src', iconsrc);
