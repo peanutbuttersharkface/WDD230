76599d8d0097b0d38684b6679767a714f9c8e695
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-03-10 11:51:51 -0700

    fixed weather

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,30 +1,34 @@
-const currentTemp = document.querySelector('#currentTemp');
+const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url ='https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=9b60de9cb85d53b6b4102f19c69a8921';
+const url =`https://api.openweathermap.org/data/2.5/weather?lat=64.837845&lon=-147.716675&appid=9b60de9cb85d53b6b4102f19c69a8921&units=imperial`
+
+
 async function apiFetch(){
     
-try{
+  try{
     const response = await fetch(url);
     if (response.ok){
         const data = await response.json();
         console.log(data);
+        
+        displayResults(data);
+      
     }else{
         throw Error(await response.text());
     }
   } catch(error){
     console.log(error);
   }
-}  
+} 
+function displayResults(weatherData){
+  currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
+
+  const iconsrc =`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;}
 apiFetch();  
 
-function displayResults(weatherData){
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-
-    const iconsrc =`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-    const desc = weatherData.weather[0].description;
-
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
-}
