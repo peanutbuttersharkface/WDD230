373904674995e30f56ef63b5f9ec90ec1e544d21
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-03-11 13:20:01 -0700

    banner

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -22,9 +22,9 @@ const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 // annoucement bar
-const banner = document.querySelector('#banner');
-const today = new Date().getDay();
 
-if (today === 1 || today === 2){
-    banner.style.display ="block";
-}
+if (day === "Monday" || day === "Tuesday");
+const ann = document.getElementById("ann");
+ann.style.display = "block";
+
+
