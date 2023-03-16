5f27c6db5b6cd85cdda3daacc844828f6cf0b8a9
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-03-15 18:41:01 -0600

    updated discover page

--- chamber/scripts/discovery.js
+++ chamber/scripts/discovery.js
@@ -34,3 +34,26 @@ imagesToLoad.forEach((img) => {
     loadImages(img);
 });
 }
+
+// number of visits user has made to page
+//const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector("#visits");
+//const date = new Date();
+//const currTime = date.getTime();
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+if(numVisits !== 0){
+  //  const lastVisitTime = localStorage.getItem('lastVisitTime');
+    //let timeDiff = currTime - lastVisitTime;
+    //let daysSinceVisit = timeDiff / (1000 * 3600 * 24);
+    visitsDisplay.textContent = numVisits;
+}else{
+    visitsDisplay.textContent = "Welcome, New member!";
+}
+
+numVisits++;
+
+localStorage.setItem("visits-ls", numVisits);
+//localStorage.setItem('lastVisitTime', currTime);
+//todayDisplay.textContent = Date.now();
