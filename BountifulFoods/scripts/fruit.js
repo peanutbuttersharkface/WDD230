51367a23531cca8e4e874b362e9101738c276f91
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-04-04 12:19:16 -0600

    fixed fruit

--- BountifulFoods/scripts/fruit.js
+++ BountifulFoods/scripts/fruit.js
@@ -1,3 +1,4 @@
+// fruit choice 1
 let dropdown = document.getElementById('fruit1');
 
 dropdown.length = 0;
@@ -7,6 +8,28 @@ defaultOption.text = 'Choice';
 
 dropdown.add(defaultOption);
 dropdown.selectedIndex = 0;
+
+//fruit choice 2
+let dropdown2 = document.getElementById('fruit2');
+
+dropdown2.length = 0;
+
+let defaultOption2 = document.createElement('option');
+defaultOption2.text = 'Choice';
+
+dropdown2.add(defaultOption2);
+dropdown2.selectedIndex = 0;
+
+//fruit choice 3
+let dropdown3 = document.getElementById('fruit3');
+
+dropdown3.length = 0;
+
+let defaultOption3 = document.createElement('option');
+defaultOption3.text = 'Choice';
+
+dropdown3.add(defaultOption3);
+dropdown3.selectedIndex = 0;
 
 const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json';
 
@@ -28,6 +51,8 @@ fetch(url)
       	  option.text = data[i].name;
       	 // option.value = data[i].abbreviation;
       	  dropdown.add(option);
+          dropdown2.add(option);
+          dropdown3.add(option);
     	}    
       });  
     }  
